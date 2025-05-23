0edba239c147bb5b7a78dfa8dd43272181e06497
froyaguilar <fdaf58@gmail.com>
2025-05-22 20:57:48 -0600

    feat: configurar proxy para llamadas API y ajustar estructura base del proyecto
    - Se agregó proxy.conf.json para redirigir /api a backend en localhost:8081
    - Se actualizó angular.json para incluir configuración de proxy
    - Se crearon archivos base: app.config.ts, app.routes.ts y main.ts
    - Se implementó servicio lab-result.service.ts con HttpClient
    - Se agregó modelo lab-result.model.ts para tipado fuerte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true, // ✅ importante aquí
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LabResultTesterComponent } from './components/lab-result-tester/lab-result-tester.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'tester', pathMatch: 'full' },
+  { path: 'tester', component: LabResultTesterComponent }
+];
